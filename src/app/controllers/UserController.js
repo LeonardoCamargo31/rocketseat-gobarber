275dfb0456e75ea2c284d9b3c069efb990955941
LeonardoCamargo31 <leonardo_camargo31@hotmail.com>
2019-11-02 11:05:38 -0300

    :sparkles: Criando usuário

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // checar se já existe usuario com esse email
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      // bad request
+      return res.status(400).json({ error: 'Usuário já existe' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,15 +1,31 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(connection) {
+    // esses campos não precisam ser um reflexo da nossa tabela db
+    // podemos ter campos virtuais, um campo que não existe do db
     const props = {
       name: Sequelize.STRING,
       email: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
       password_hash: Sequelize.STRING,
       provider: Sequelize.BOOLEAN,
     };
     // chamando o método init do Model, e passando a conexão
     super.init(props, { sequelize: connection });
+
+    // trechos de códigos executados baseado em ações
+    // esse é executado antes de salvar(create a update)
+    this.addHook('beforeSave', async user => {
+      // sempre que password ser informado
+      if (user.password) {
+        // hash(senha,numero de rounds)
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    // sempre retorno o model que foi inicializado
+    return this;
   }
 }
 
